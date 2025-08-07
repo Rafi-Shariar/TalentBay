777e2d1c0d4b8fdcf659b77692d98502dbe3a6d3
Rafi-Shariar <rafi.shariar619@gmail.com>
2025-08-07 12:05:53 +0600

    project setup

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,21 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+
+export const collectionNames = {
+    userCollection : 'users',
+
+}
+
+export default function dbConnect(collectionName : string) {
+  const uri = process.env.MONGODB_URI as string;
+
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+
+  return client.db(process.env.DB_NAME).collection(collectionName);
+}
